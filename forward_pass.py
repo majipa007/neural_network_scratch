f4cf2eead1e497c056c48c420f5d2e5de1e8fa69
sulav kumar shrestha <sulavstha007@gmail.com>
2024-08-24 13:23:15 +0530

    random weight updation

--- forward_pass.py
+++ forward_pass.py
@@ -4,7 +4,7 @@ class Layer_Dense:
   def __init__(self, n_inputs, n_neurons):
     self.output = None
     self.weights = 0.01 * np.random.uniform(-1, 1, (n_inputs, n_neurons))
-    self.biases = np.random.uniform(-1, 1,n_neurons)
+    self.biases = np.random.uniform(-1, 1,(1,n_neurons))
   def forward(self, inputs):
     self.output = inputs@self.weights + self.biases
     return self.output
--- main.py
+++ main.py
@@ -14,28 +14,62 @@ loss_fuction = Categorical_Cross_Entropy()
 
 #--------------------------defining the network-------------------------------
 
-dense1 = Layer_Dense(2, 126)
-dense2 = Layer_Dense(126, 512)
-dense3 = Layer_Dense(512, 3)
+dense1 = Layer_Dense(2, 3)
+dense2 = Layer_Dense(3, 4)
+dense3 = Layer_Dense(4, 3)
+
+#--------------------------initializing values-------------------------------
+
+X, y = spiral_data(samples=10, classes=3)
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense3_weights = dense3.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_biases = dense2.biases.copy()
+best_dense3_biases = dense3.biases.copy()
 
 #--------------------------feed forward-------------------------------
 
+for iteration in range(100000):
+    dense1.weights+=0.05*np.random.randn(2, 3)
+    dense2.weights+=0.05*np.random.randn(3, 4)
+    dense3.weights+=0.05*np.random.randn(4, 3)
+    dense1.biases+=0.05*np.random.randn(1, 3)
+    dense2.biases+=0.05*np.random.randn(1,4)
+    dense3.biases+=0.05*np.random.randn(1,3)
 
-X, y = spiral_data(samples=10, classes=3)
-dense1.forward(X)
-relu.forward(dense1.output)
-dense2.forward(relu.output)
-relu.forward(dense2.output)
-dense3.forward(relu.output)
-sm.forward(dense3.output)
+    dense1.forward(X)
+    relu.forward(dense1.output)
+    dense2.forward(relu.output)
+    relu.forward(dense2.output)
+    dense3.forward(relu.output)
+    sm.forward(dense3.output)
 
 
 #---------------------------loss and accuracy-------------------------------------
 
-loss = loss_fuction.calculate(sm.output,y)
-acc = accuracy(sm.output, y)
-print("Accuracy", acc)
-print("Loss", loss)
-print("output", sm.output)
-print("truth", y)
+    loss = loss_fuction.calculate(sm.output,y)
+    acc = accuracy(sm.output, y)
 
+    # print("output", sm.output)
+    # print("truth", y)
+
+#---------------------------random weight update-------------------------------------
+    if loss < lowest_loss:
+        best_dense1_weights = dense1.weights.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense3_weights = dense3.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_biases = dense2.biases.copy()
+        best_dense3_biases = dense3.biases.copy()
+        lowest_loss = loss
+        print("Accuracy", acc)
+        print("Loss", loss)
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense3.weights = best_dense3_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.biases = best_dense2_biases.copy()
+        dense3.biases = best_dense3_biases.copy()
